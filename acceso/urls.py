768c957315044777795a479275218a57652ec0dc
YashinCA <yashin.alamos@gmail.com>
2022-04-05 22:46:44 -0400

    Se agrega modelo imagen a Admin y se modifica funcion str del modelo imagen

--- acceso/urls.py
+++ acceso/urls.py
@@ -12,7 +12,6 @@ urlpatterns = [
     path('', welcome, name='bienvenida'),
     path('acceso/', LoginView.as_view(), name='acceso'),
     path('talleres', Talleres.as_view(), name="talleres"),
-    #path('talleres/', Talleres.as_view(), name='talleres'),
     path('view/<int:pk>', Detail.as_view(), name="detail"),
     path('login/', login, name='login'),
     path('forget_password/', forgetpassword, name='forgetpassword'),
