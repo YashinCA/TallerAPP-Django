18f0c6450f3d2c005b977c7b24b4a52aeea142ea
YashinCA <yashin.alamos@gmail.com>
2022-05-11 23:07:04 -0400

    Se generan cambios

--- proyecto_base/urls.py
+++ proyecto_base/urls.py
@@ -14,8 +14,3 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL,
            document_root=settings.MEDIA_ROOT)
-
-# if settings.DEBUG:
-#     urlpatterns += static(settings.MEDIA_URL,
-#                           document_root=settings.MEDIA_ROOT)
-##################
